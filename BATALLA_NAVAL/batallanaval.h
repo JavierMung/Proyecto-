1aa0ee06e3defaaa021e13d55f6f125714f59879
Moises-C <cruz.ms97@gmail.com>
2019-11-27 10:20:12 -0600

    Se agregan nuevos prototipos de funciones

--- BATALLA_NAVAL/batallanaval.h
+++ BATALLA_NAVAL/batallanaval.h
@@ -9,7 +9,7 @@ struct Matriz
 {
   int x;
   int y;
-  int aciertos;
+  int datoex;
   int dato;
   struct Matriz *siguiente;
   struct Matriz *anterior;
@@ -18,20 +18,26 @@ struct Matriz
 struct Matriz *crearmatriz (struct Matriz *);
 struct Matriz *generarcoordenadas (struct Matriz *);
 struct Matriz *insertar (struct Matriz *, int, int, int);
-struct Matriz *ColocarArriba (struct Matriz *, int, int, int, int);
-struct Matriz *ColocarAbajo (struct Matriz *, int, int, int, int);
-struct Matriz *ColocarIzquierda (struct Matriz *, int, int, int, int);
-struct Matriz *ColocarDerecha (struct Matriz *, int, int, int, int);
+struct Matriz *ColocarArriba (struct Matriz *, int, int, int, int, int);
+struct Matriz *ColocarAbajo (struct Matriz *, int, int, int, int, int);
+struct Matriz *ColocarIzquierda (struct Matriz *, int, int, int, int, int);
+struct Matriz *ColocarDerecha (struct Matriz *, int, int, int, int, int);
 struct Matriz *tablero (struct Matriz *);
-void mostrar (struct Matriz *);
 struct Matriz *coordenadas (struct Matriz *, int, int);
 struct Matriz *comenzar (struct Matriz *, int);
-void menu ();
 struct Matriz *colocacion (struct Matriz *, int);
+struct Matriz *aleatorios (struct Matriz *, int);
+struct Matriz *ledio (struct Matriz *, struct Matriz *, int, int, int);
+struct Matriz *coordenadasatacar (struct Matriz *, struct Matriz *, int);
+void iniciar (struct Matriz *, struct Matriz *, int);
+int gano (struct Matriz *);
+void mostrar (struct Matriz *, int);
+void menu ();
 void instrucciones ();
-struct Matriz *aleatorios (struct Matriz *, int );
 void vs ();
 void campodebatalla (struct Matriz *);
-void recolocar (struct Matriz *, int);
-void tiro(struct Matriz *, int , int);
+void recolocar (struct Matriz *, int, int);
+void dio (struct Matriz *, struct Matriz *, int);
+void fallo ();
+void gane_ ();
 #endif
